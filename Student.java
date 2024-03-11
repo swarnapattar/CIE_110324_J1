41e90b85ee3b175969564c87017cf069e400c89e
Swarna <01fe22bca020@kletech.ac.in>
2024-03-11 15:55:36 +0530

    This program has Student details

---
+++ Student.java
@@ -0,0 +1,24 @@
+public class Student extends Course{
+    String name="Swarna";
+    String department="BCA";
+    String program="BCA";
+    public String RegistertedCourse[]={" ","C#","Applied statistics","Java"};
+
+    public void display(){
+        System.out.println("----Student Details----");
+        System.out.println("Name:"+name);
+        System.out.println("Department: "+department);
+        System.out.println("Program:"+program);
+        System.out.print("Registered Courses:\n");
+        for(int i=1;i<=3;i++){
+            System.out.println(i+":"+RegistertedCourse[i]);
+        }
+    }
+
+    public static void main(String[] args){
+        Student s = new Student();
+        s.display();
+        s.DisplayMarks();
+    
+    }
+}
